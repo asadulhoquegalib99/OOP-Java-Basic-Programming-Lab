8292dc120c983643ce1bb4110ff9c985da4f5452
Asadul Hoque Galib <asadulhoquegalib99@gmail.com>
2026-08-06 17:44:48 +0600

    Update and rename 8.PatternDisplay.java to PatternDisplay8.java

--- PatternDisplay8.java
+++ PatternDisplay8.java
@@ -1,4 +1,4 @@
-public class PatternDisplay {
+public class PatternDisplay8 {
     public static void main(String[] args) {
         System.out.println("   J    a   v     v  a");
         System.out.println("   J   a a   v   v  a a");
