b103b665b7c32d5d3f26740ece22baff5994e063
Asadul Hoque Galib <asadulhoquegalib99@gmail.com>
2026-08-06 17:44:09 +0600

    Update and rename 7. Multiplication Table to MultiplicationTable7.java

--- MultiplicationTable7.java
+++ MultiplicationTable7.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MultiplicationTable {
+public class MultiplicationTable7 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
